709f801bb6e48848f1cd22d8a3f570e87e31e090
kssushas <ksuysha.kovtun@gmail.com>
2023-12-16 13:35:16 +0200

    initial commit

---
+++ src/components/FindName.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="find">
+   <p>I will find u!</p>
+   <input type="text" v-model="search" placeholder="enter names">
+   <p>{{ information }}</p>
+   <ul>
+    <li v-for="name in filteredNames" :key="name">{{ name }}</li>
+   </ul>
+
+  </div>
+</template>
+
+<script>
+ export default{
+    name: 'FindName',
+    data(){
+        return {
+            search : '',
+            coincidence: 0,
+            names : ['Vanya', 'Ks', 'Ira', 'Dima', 'Valeriy'],
+        }
+    },
+    computed: {
+        information(){
+            return `Amount of names: ${this.names.length} Coincidence: ${this.filteredNames.length}`;
+        },
+        filteredNames(){
+            return this.names.filter(name => name.toLowerCase().startsWith(this.search.toLowerCase()));
+        },
+    }
+ }
+</script>
---
+++ src/components/MyComponents.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>Enter the value</p>
+    <button @click="clickOnMin">-</button>
+    <select v-model="selectedValue">
+      <option v-for="val in data" :key="val" :value="val">{{ val }}</option>
+    </select>
+    <button @click="clickOnMax">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data() {
+    return {
+      data: [0, 1, 2, 3],
+      selectedValue: 0,
+    };
+  },
+  methods: {
+    clickOnMin() {
+      if (this.selectedValue > 0){
+         this.selectedValue = this.selectedValue - 1;
+      }
+    },
+    clickOnMax() {
+      if (this.selectedValue < this.data[this.data.length-1]){
+
+      this.selectedValue = this.selectedValue + 1;
+      }
+    },
+  },
+};
+</script>
